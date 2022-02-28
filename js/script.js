33bd9886d9f0b86e48560ee38ba03692bb52f198
Коновалов Данил Сергеевич <konovalov@elewise.local>
2022-02-28 14:00:41 +0400

    asdas

--- js/script.js
+++ js/script.js
@@ -128,25 +128,6 @@ $(document).ready(function () {
 
 
 });
-// document.getElementById("horizontal-scroller")
-//     .addEventListener('wheel', function (event) {
-//         if (event.deltaMode == event.DOM_DELTA_PIXEL) {
-//             var modifier = 1;
-//             // иные режимы возможны в Firefox
-//         } else if (event.deltaMode == event.DOM_DELTA_LINE) {
-//             var modifier = parseInt(getComputedStyle(this).lineHeight);
-//         } else if (event.deltaMode == event.DOM_DELTA_PAGE) {
-//             var modifier = this.clientHeight;
-//         }
-//         if (event.deltaY != 0) {
-//             // замена вертикальной прокрутки горизонтальной
-//             this.animate({scrollLeft: modifier * event.deltaY}, 100);
-//
-//             this.scrollLeft += modifier * event.deltaY;
-//             event.preventDefault();
-//         }
-//     });
-
 //Скролл по пунктам страницы
 
 function yScroll() {
